d9a85514ebbda9496121bbf7a7dd30f3917ff3c9
Süleyman Burak Yaşar <91782773+4o4x@users.noreply.github.com>
2023-07-18 13:40:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def revoke_ovpn_config(id):
     # Menüdeki seçenekleri taramak için bir döngü başlatın
     while True:
         # Menüden çıktıyı alın ve satırlara bölelim
-        menu_output = process.before.splitlines()
+        menu_output = process.after.splitlines()
 
         # Her satırı kontrol edelim
         for line in menu_output:
