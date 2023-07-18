e7849e4f068fa6c325697136f6a9e73525bd1f6c
Süleyman Burak Yaşar <91782773+4o4x@users.noreply.github.com>
2023-07-18 13:18:47 +0300

    bug fix

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def revoke_ovpn_config(id):
     process = pexpect.spawn("sudo bash /home/openvpn-install.sh", encoding="utf-8")
     process.sendline("2")
     process.expect("Select the client to revoke:")
-    print(process.before)
+    #print(process.before)
     process.sendline(id)
     process.sendline("y")
     process.sendeof()
